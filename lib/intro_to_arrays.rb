3d0724a8210a6662d76d47f05dd7604e8ebc3b70
John Martinez <jmartinez729@gmail.com>
2018-10-23 20:39:17 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,4 +5,21 @@ def instantiate_new_array
 end 
 
 def array_with_two_elements
-  
+  array2 = [1, 2]
+  return array2
+end 
+
+def first_element(array)
+  return array[0]
+end 
+
+def third_element(array)
+  return array[2]
+end 
+
+def last_element(array)
+  return array[-1]
+end 
+
+def first_element_with_array_methods(array)
+  return array.first
